eb4812b9254f0fa311ff6796ccb61ad9256d55bd
Aaron Dalton <aaron@daltons.ca>
2022-05-18 21:22:55 -0600

    Adding a table

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <main>
-    <section class="section content" id="introText">
+    <section class="section id-medium content" id="introText">
         <p>This is a list of empirical research related to topics of interest to editors.</p>
 
         <p>Some of editing is purely arbitrary (style), but most of it is rooted in not just grammar but what we have learned about how readers receive different types of material. When defending what might feel to an author as an arbitrary call, it can be helpful to have robust, defensible research to back you up. Hopefully this will help.</p>
@@ -12,11 +12,33 @@
 
         <hr>
     </section>
-    <section class="section content">
+    <section class="section id-medium content">
         {#if $research != null}
             <p>{$research.length} records found.</p>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Year</th>
+                        <th>Authors</th>
+                        <th>Abstract</th>
+                        <th>Date added</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {#each $research as rec (rec.title)}
+                        <tr>
+                            <td>{rec.title}</td>
+                            <td>{rec.year}</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp</td>
+                            <td>{rec.dateAdded}</td>
+                        </tr>
+                    {/each}
+                </tbody>
+            </table>
         {:else}
-            <p>Loading data...</p>
+            <p>Error fetching data</p>
         {/if}
 
     </section>
